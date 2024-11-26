97e8425a11009dd22eb30d770ffa77fc86b42f91
zerock <zk2020308a@gmail.com>
2024-11-26 16:16:31 +0900

    host open

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:
+      {
+        host: "0.0.0.0", // 모든 네트워크 인터페이스에서 접속 허용
+        port: 5173,      // 기본 포트 (변경 가능)
+      },
 })
